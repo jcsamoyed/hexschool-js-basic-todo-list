720850173b8009c480e0a036e4a612eceb3d11c2
jcsamoyed <aruby5223@gmail.com>
2021-10-18 01:05:00 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import css from "./style.scss";
+
+console.log("從src/index.js編譯出來的資料");
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
