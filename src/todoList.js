48e7ee683524c4412d0a09de6cd47326bfdcad6e
jcsamoyed <aruby5223@gmail.com>
2021-10-18 18:06:54 +0800

    feat: Add new todo function

--- src/todoList.js
+++ src/todoList.js
@@ -1,25 +1,40 @@
+const todo = document.querySelector(".card__input");
+const addBtn = document.querySelector(".card__add-btn");
+
 let data = [
   {
-    id: 1,
+    id: "t1",
     content: "準備面試資料",
   },
   {
-    id: 2,
+    id: "t2",
     content: "乖乖被洗臉",
   },
   {
-    id: 3,
+    id: "t3",
     content: "認命回來上課",
   },
   {
-    id: 4,
+    id: "t4",
     content: "做 todoList 卡關",
   },
   {
-    id: 5,
+    id: "t5",
     content: "爬文求救",
   },
 ];
+
+addBtn.addEventListener("click", function () {
+  if (!todo.value) {
+    return;
+  }
+  let newTodo = {};
+  newTodo.id = Date.now();
+  newTodo.content = todo.value;
+  data.push(newTodo);
+  renderData();
+  todo.value = "";
+});
 
 function renderData() {
   let str = "";
@@ -35,5 +50,4 @@ function renderData() {
   const list = document.querySelector(".card__list");
   list.innerHTML = str;
 }
-
 renderData();
